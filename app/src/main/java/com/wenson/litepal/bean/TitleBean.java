d487b868c6d70001576f7d150e6878a282d2c5c5
luowangcheng <wenson_luo@163.com>
2017-10-30 19:49:36 +0800

    gaibi (reverted from commit 9a3b507ef9ac46fc7754ef2f129f232b9571a50a)

---
+++ app/src/main/java/com/wenson/litepal/bean/TitleBean.java
@@ -0,0 +1,21 @@
+package com.wenson.litepal.bean;
+
+/**
+ * Created by Wenson_Luo on 2017/10/28.
+ */
+
+public class TitleBean {
+    private String title;
+
+    public TitleBean(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+}
